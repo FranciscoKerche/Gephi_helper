bd0cebfb15ed617ab8fec2dd414981ef5aae1b44
FranciscoKerche <chicowkn@gmail.com>
2020-12-18 15:17:19 -0300

    Added a simple variable, for when someone wants only to see the nodes that are present in more than one structure in the data. For instance, with we are correlating players through time, a player it would filter players that had only been in one team. This way modularity won't grasp only the difference of events

--- list_to_copresence.R
+++ list_to_copresence.R
@@ -1,13 +1,27 @@
-copresence_gephi <- function(base, copresence, by){
+copresence_gephi <- function(base, copresence, by, simple = F){ #erro arg must be a symbol
   `%>%` <- magrittr::`%>%`
+  if(simple == T){
+    basic <- base %>%
+      group_by({{copresence}}, {{by}}) %>%
+      summarise() %>%
+      ungroup() %>%
+      group_by({{copresence}}) %>%
+      summarise(count = n()) %>%
+      filter(count > 1) %>%
+      select(f = {{copresence}})
+    base %>%
+      filter({{copresence}} %in% basic$f) %>%
+      group_by({{by}}) %>%
+      summarise(gephi = paste({{copresence}}, collapse = ","))
+  } else {
   base %>%
     dplyr::group_by({{ by }}) %>%
     dplyr::summarise(relations = paste({{ copresence }}, collapse = ","))
-}
+}}
 
 #This function is to be used with the gephi package convert excel and csv files into network with both type of agents beeing "relations", and clicking to have connection between relations and relations.
 
-
+#For each value:
 #base : The data-set to be used
 #copresence : the variable you want to be associated by copresence, for instance, if you want to see the copresence of students in a class, you should put students here
 #by : by wich value should it be copresent, for instance, if you want to see the copresence of students in a classroom, you should put the class-room here
